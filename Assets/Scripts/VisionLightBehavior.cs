2e5951540aab88b40a320a65d1e2470596fedf04
Nathan Dykema <nathandykema@gmail.com>
2026-04-01 19:56:57 -0400

    Clarify comments in VisionLightBehavior script
    Updated comments in VisionLightBehavior for clarity.

--- Assets/Scripts/VisionLightBehavior.cs
+++ Assets/Scripts/VisionLightBehavior.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is a placeholder for handling vision-based light behavior.
+// It currently contains empty Start and Update methods for future implementation.
 public class VisionLightBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Called before the first frame update
     void Start()
     {
-        
+        // Initialization logic for vision light behavior can be added here
     }
 
-    // Update is called once per frame
+    // Called once per frame
     void Update()
     {
-        
+        // Per-frame logic for vision light behavior can be added here
     }
 }
