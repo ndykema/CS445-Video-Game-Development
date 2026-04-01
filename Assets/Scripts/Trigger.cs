940a2a2c6241425e18f4f05e8b2051e45d51eeea
Nathan Dykema <nathandykema@gmail.com>
2026-04-01 19:56:30 -0400

    Refactor comments in Trigger.cs for better clarity
    Updated comments for clarity and consistency.

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script responds to collisions with the object it is attached to.
+// When a collision occurs, it changes the object's color and logs a message.
 public class Trigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Called automatically when this object collides with another object
     void OnCollisionEnter(Collision collision)
     {
+        // Change the material color of this object's MeshRenderer to red
         GetComponent<MeshRenderer>().material.color = Color.red;
+
+        // Output a message to the Unity Console indicating a collision occurred
         Debug.Log("Something hit me!");
-
-
     }
 }
